cc09b2e3ece42826610bde17d8a43c3d7398ab51
minsoo cho <khg04170@gmail.com>
2022-02-15 11:43:10 +0000

    habit component in progress...........

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -3,27 +3,30 @@ import React, { Component } from 'react';
 class Habit extends Component {
     state = {
       count: 0,
-      };
-    handleIncrease = () => {
-      this.setState({ count: this.state.count + 1})
-      };
-    handleDecrease = () => {
-      const count = this.state.count - 1;
-      this.setState({ count: count < 0 ? 0 : count})
-      };
+    };
+
+  handleIncrease = () => {
+    this.props.onIncrease(this.props.habit)
+    };
+  handleDecrease = () => {
+    this.props.onDecrease(this.props.habit)
+    };
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit)
+    };
   render() {
-    const { name } = this.props.habit
+    const { name, count } = this.props.habit;
     return <>
       <li className="habit">
           <span className="habit-name">{name}</span>
-          <span className="habit-count">{this.state.count}</span>
+          <span className="habit-count">{count}</span>
           <button className ="habit-btn habit-increase" onClick ={this.handleIncrease}>
             <i className="fas fa-plus"></i>
           </button>
           <button className ="habit-btn habit-decrease" onClick ={this.handleDecrease}>
             <i className="fas fa-minus"></i>
           </button>
-          <button className ="habit-btn habit-delete">
+        <button className="habit-btn habit-delete" onClick={this.handleDelete}>
             <i className="fas fa-trash"></i>
           </button>
       </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -7,14 +7,35 @@ import Habit from './habit';
       { id: 1, name: 'Reading', count: 0 },
       { id: 2, name: 'Running', count: 0 },
       { id: 3, name:'Coding', count: 0 }
-    ],
-    nationality: ["korea", "uk"]
+    ]
   };
+
+  handleIncrease = (habit) => {
+    const habits = [...this.state.habits]
+    const index = habits.indexOf(habit)
+    habits[index].count++;
+    this.setState({ habits });
+    };
+  handleDecrease = (habit) => {
+    const habits = [...this.state.habits]
+    const index = habits.indexOf(habit)
+
+    this.setState({ habits });
+    };
+  handleDelete = (habit) => {
+    console.log(`delete ${habit}`)
+    };
+
   render() {
     return (
     <ul>
       {this.state.habits.map(habit => (
-        <Habit key={habit.id} habit={habit}/>
+        <Habit
+          key={habit.id}
+          habit={habit}
+          onIncrease={this.handleIncrease}
+          onDecrease={this.handleDecrease}
+          onDelete={this.handleDelete} />
       ))}
 
     </ul>
