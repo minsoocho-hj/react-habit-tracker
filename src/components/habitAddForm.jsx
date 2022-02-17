fe5f312e8561022f73fbce943d62178e5a31661b
minsoo cho <khg04170@gmail.com>
2022-02-17 22:00:21 +0000

    catchup commit

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default class HabitAddForm extends Component {
-  inputRef = React.createRef();
+  inputRef = React.createRef(); //ref 리액트기능 document ~~value해서 값 받아오는것 대신.브라우저에서 돔요소에 접근하지않고, 필요시에는 리액트 제공 createref통해 멤버변수 정한다음, 해당하는 ref연결하면된다.
   render() {
     return (
       <form className="add-form" onSubmit={this.props.onSubmit}>
